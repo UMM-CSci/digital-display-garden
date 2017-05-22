47d769ad2129eecc2a215b070c5c2c145935e88a
danfrz <dansaoficial@gmail.com>
2017-05-22 13:17:33 -0500

    Bring over DeleteComponent from dorfnerv2
    Credit to dorfnerv2-digital-display-garden
    https://github.com/UMM-CSci-3601-S17/digital-display-garden-iteration-4-dorfner-v2.git

--- client/src/app/admin/src/admin.service.ts
+++ client/src/app/admin/src/admin.service.ts
@@ -23,6 +23,10 @@ export class AdminService {
         return this.http.request(this.url + "admin/liveUploadId").map(res => res.json());
     }
 
+    deleteUploadID(uploadID : string) : Observable<any> {
+        return this.http.delete(this.url + "deleteData/" + uploadID, {withCredentials: true}).map(res => res.json());
+    }
+
     //Google Charts HTTP requests
 
     getViewsPerHour(): Observable<any[][]> {
---
+++ client/src/app/admin/tests/delete.component.spec.ts
@@ -0,0 +1,74 @@
+import {ComponentFixture, TestBed, async} from "@angular/core/testing";
+import { Observable } from "rxjs";
+import {FormsModule} from "@angular/forms";
+import {DeleteComponent} from "../src/delete.component";
+import {AdminService} from "../src/admin.service";
+import {RouterTestingModule} from "@angular/router/testing";
+
+describe("Delete Component", () => {
+
+    let deleteComponent: DeleteComponent;
+    let fixture: ComponentFixture<DeleteComponent>;
+    let adminServiceStub: {
+        getUploadIDs: () => Observable<string[]>,
+        getLiveUploadID: () => Observable<string>,
+        deleteUploadID: (string) => Observable<any>,
+        authorized: () => Observable<boolean>
+    };
+
+    beforeEach(() => {
+        adminServiceStub = {
+            getUploadIDs: () => {
+                return Observable.of(["upload id 1", "upload id 2"]);
+            },
+            getLiveUploadID: () => {
+                return Observable.of("upload id 2");
+            },
+            deleteUploadID: (uploadID: string) => {
+
+                return Observable.of({
+                    success: ["upload id 1", "upload id 2"].filter(str => str !== uploadID).length === 1,
+                    uploadIDs: ["upload id 1", "upload id 2"].filter(str => str !== uploadID)
+                });
+            },
+            authorized: () => {
+                return Observable.of(true);
+            }
+        };
+
+        TestBed.configureTestingModule({
+            imports: [FormsModule, RouterTestingModule ],
+            declarations: [ DeleteComponent],
+            providers:    [{provide: AdminService, useValue: adminServiceStub}]
+        });
+
+    });
+
+    beforeEach(
+        async(() => {
+            TestBed.compileComponents().then(() => {
+                fixture = TestBed.createComponent(DeleteComponent);
+                deleteComponent = fixture.componentInstance;
+                fixture.detectChanges();
+            });
+    }));
+
+    it("can be initialized", () => {
+        expect(deleteComponent).toBeDefined();
+    });
+
+    it("initializes the authorized field", () => {
+        expect(deleteComponent.authorized).toEqual(true);
+    });
+
+    it("can delete an uploadID", () => {
+       deleteComponent.delete("upload id 1");
+       expect(deleteComponent.uploadIDs).toEqual(["upload id 2"]);
+    });
+
+    it("changes nothing on failed deletions", () => {
+       deleteComponent.delete("blablabla");
+       expect(deleteComponent.uploadIDs).toEqual(["upload id 1", "upload id 2"]);
+    });
+
+});
