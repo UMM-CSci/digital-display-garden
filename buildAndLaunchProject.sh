c171b064cb55bbf45db4db91f4d19e1c05c947af
= <dansaoficial@gmail.com>
2017-07-21 22:19:18 -0500

    Fix build and launch script

--- buildAndLaunchProject.sh
+++ buildAndLaunchProject.sh
@@ -18,7 +18,7 @@ cp server/build/distributions/server.tar "~/server_$1.tar"
 tar xvf "~/server_$1.tar" -C ~
 sleep 1
 
-mv server "server_$1"
+mv ~/server "~/server_$1"
 
 #Maintain the configuration and authentication files
 AUTH_USERS_PATH="`pwd`/server/authorized.users"
