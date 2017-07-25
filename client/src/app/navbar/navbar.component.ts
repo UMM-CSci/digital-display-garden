b745231eb549a1524e233fd02214b0c22d91def4
danfrz <dansaoficial@gmail.com>
2017-07-25 01:32:52 -0500

    Make navbar clear search filter when clicked

--- client/src/app/navbar/navbar.component.ts
+++ client/src/app/navbar/navbar.component.ts
@@ -1,8 +1,22 @@
 import { Component } from '@angular/core';
+import {PlantListService} from "../garden/components/plant_list/src/plant-list.service";
 
 @Component({
     selector: 'navbar-component',
     templateUrl: 'navbar.component.html'
 })
 
-export class NavbarComponent {}
+export class NavbarComponent {
+
+    constructor (private plantListService: PlantListService)
+    {
+
+    }
+
+
+    private clearSearchTerms()
+    {
+        this.plantListService.setSearchTerms("");
+    }
+}
+
